c1d6fc4d7afd38f2db6ea4fc14246d4381069f59
gamerboy404 <37173930+gamerboy404@users.noreply.github.com>
2019-07-17 11:25:28 -0400

    Update Niel_Yashodhar.cs
    Code added

--- Niel_Yashodhar.cs
+++ Niel_Yashodhar.cs
@@ -8,6 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hey It's Niel and It Works");
+        Debug.Log("it still does");
     }
 
     // Update is called once per frame
